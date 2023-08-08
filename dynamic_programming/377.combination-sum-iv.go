7de626559efd1f3d4b361510748de94a1fd54662
tekwell1427 <tekwell1427@gmail.com>
2023-08-08 19:51:14 +0800

    medium

---
+++ dynamic_programming/377.combination-sum-iv.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+ * @lc app=leetcode id=377 lang=golang
+ *
+ * [377] Combination Sum IV
+ */
+
+// @lc code=start
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+
+	// bagsize first!
+	for curTarget := 0; curTarget <= target; curTarget++ {
+		// get maximum number of arragement in curTarget
+		for _, num := range nums {
+			if curTarget >= num {
+				dp[curTarget] += dp[curTarget-num]
+			}
+		}
+	}
+
+	return dp[target]
+}
+
+// @lc code=end
