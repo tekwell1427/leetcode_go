6fd93aa1e478c770761a7db28687049b2c8227c3
tekwell1427 <tekwell1427@gmail.com>
2023-08-12 23:34:05 +0800

    medium

--- greedy/45.jump-game-ii.go
+++ greedy/45.jump-game-ii.go
@@ -8,27 +8,19 @@ package main
 
 // @lc code=start
 func jump(nums []int) int {
-	if len(nums) < 2 {
-		return 0
-	}
-	// sure to reach desination now
-	count := 0
-	maxIdx := nums[0] + 0
-	checkPointIdx := nums[0] + 0
-	for i, num := range nums {
-		if checkPointIdx >= len(nums)-1 {
-			count++
-			break
-		}
-		curMaxIdx := num + i
-		maxIdx = max(maxIdx, curMaxIdx)
-		if checkPointIdx == i {
-			count++
-			checkPointIdx = maxIdx
-		}
-	}
+	curJumpMaxIdx := 0
+	nextJumMaxIdx := 0
+	numJumps := 0
 
-	return count
+	for i := 0; i < len(nums); i++ {
+		nextJumMaxIdx = max(nextJumMaxIdx, i+nums[i])
+		if i == curJumpMaxIdx && i != len(nums)-1 {
+			numJumps++
+			curJumpMaxIdx = nextJumMaxIdx
+			nextJumMaxIdx = 0
+		}
+	}
+	return numJumps
 }
 func max(a, b int) int {
 	if a > b {
