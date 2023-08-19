3ed16df4ecb18d49d6887315aaff038e389c201b
tekwell1427 <tekwell1427@gmail.com>
2023-08-19 11:39:20 +0800

    medium

---
+++ linked_list/24.swap-nodes-in-pairs.go
@@ -0,0 +1,34 @@
+package main
+
+/*
+ * @lc app=leetcode id=24 lang=golang
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	dummy := &ListNode{}
+	pre := dummy
+
+	for head != nil && head.Next != nil {
+		next := head.Next // 2
+		pre.Next = next   // 0 2
+		newHead := next.Next
+		next.Next = head
+		pre = head
+		head = newHead
+
+	}
+	pre.Next = head
+	return dummy.Next
+}
+
+// @lc code=end
