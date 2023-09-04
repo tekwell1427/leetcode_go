044b4ac758123f5910b3bb4649b9821009c8e954
tekwell1427 <tekwell1427@gmail.com>
2023-09-04 21:21:53 +0800

    add worst/best case

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -2,20 +2,31 @@ package main
 
 import "fmt"
 
-func BubbleSort(s []int) {
+func BubbleSort(s []int) int {
+	count := 0
 	for n := 0; n < len(s); n++ {
+		ischange := false
 		for i := 0; i < len(s)-1; i++ {
+			count++
 			if s[i] > s[i+1] {
 				tmp := s[i]
 				s[i] = s[i+1]
 				s[i+1] = tmp
+				ischange = true
 			}
 		}
+		if !ischange {
+			break
+		}
 	}
+	return count
 }
 
 func main() {
-	a := []int{1, 5, 7, 8, 4, 9, 10}
-	BubbleSort(a)
-	fmt.Println(a)
+	a := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	count := BubbleSort(a)
+	fmt.Println("worst case: ", a, "count:", count)
+	a = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	count = BubbleSort(a)
+	fmt.Println("best case: ", a, "count:", count)
 }
