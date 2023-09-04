101aae6a3b17b537e70442b12704063affa3e6a2
tekwell1427 <tekwell1427@gmail.com>
2023-09-04 21:18:06 +0800

    add worst/best case

--- sort/insertion_sort.go
+++ sort/insertion_sort.go
@@ -2,20 +2,29 @@ package main
 
 import "fmt"
 
-func InsertionSort(s []int) {
-	for n := 1; n < len(s); n++ {
+func InsertionSort(s []int) int {
+	count := 0
+	// for n := len(s) - 1; n >= 0; n-- {
+	for n := 0; n < len(s); n++ {
+		cur := s[n]
 		for i := n - 1; i >= 0; i-- {
-			if s[i+1] < s[i] {
-				tmp := s[i+1]
+			count++
+			if cur < s[i] {
 				s[i+1] = s[i]
-				s[i] = tmp
+				s[i] = cur
+			} else {
+				break
 			}
 		}
 	}
+	return count
 }
 
 func main() {
-	a := []int{1, 5, 7, 8, 4, 9, 10}
-	InsertionSort(a)
-	fmt.Println(a)
+	a := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	count := InsertionSort(a)
+	fmt.Println("worst case: ", a, "count:", count)
+	a = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	count = InsertionSort(a)
+	fmt.Println("best case: ", a, "count:", count)
 }
