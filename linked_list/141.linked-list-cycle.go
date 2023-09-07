4b589726903477e88583aeebb053027a8ecdaee5
tekwell1427 <tekwell1427@gmail.com>
2023-09-07 20:55:41 +0800

    easy

---
+++ linked_list/141.linked-list-cycle.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+// @lc code=end
